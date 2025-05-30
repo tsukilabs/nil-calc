179bd74eb2b0cac12060a1555d7840b1ff66e778
Andrew Ferreira <andrew.shien2@gmail.com>
2025-05-29 21:35:10 -0300

    ci: add install param

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -1,8 +1,16 @@
+param(
+  [switch]$Install
+)
+
 $ErrorActionPreference = 'Stop'
 $PSNativeCommandUseErrorActionPreference = $true
 
 Get-ChildItem 'core/pkg' -Recurse | Remove-Item
 
 wasm-pack build core --release
+
+if ($Install) {
+  pnpm install
+}
 
 pnpm run -F web build
